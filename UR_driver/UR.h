b2ce923093f1844c7087c94f3138db05ed5cb6ce
yomo <hoshihanabi08@gmail.com>
2023-01-06 15:35:45 +0900

    add all files

--- UR_driver/UR.h
+++ UR_driver/UR.h
@@ -38,7 +38,6 @@ public:
 	void movel(double x, double y, double z, double a, double b, double c, double tt);
 	void movej(double j1, double j2, double j3, double j4, double j5, double j6, double tt);
 	void movel_add(double x, double y, double z, double a, double b, double c, double tt);
-	//void movel_vf(double x, double y, double z, double a, double b, double c, double tt);
 	void read();
 
 private:
